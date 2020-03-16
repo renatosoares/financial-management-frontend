a76eeef0801da4ea146edab5288162c20ea13148
Renato Soares <renatosoarespro@hotmail.com>
2020-03-16 09:44:38 -0300

    define default methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Statement></Statement>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Statement from './components/Statement.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Statement,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Statement.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+      <section>section1</section>
+      <section>section2</section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Statement extends Vue {
+    private statement: object = {
+        name: null,
+        type: null,
+        amount: null,
+        amountAt: null,
+    };
+    private statementCollection: Array<object> = [];
+
+    get statements(): Array<object> {
+        return this.statementCollection;
+    }
+    set statements(newValue: Array<object>) {
+        this.statementCollection.push(newValue);
+    }
+
+    @Watch('statement', {
+        deep: true,
+    })
+    statementChanged(newVal: object, oldVal: object) {
+        // TODO
+    }
+
+    public store(): void {
+        // TODO
+    }
+
+    public destroy(index: number): void {
+        // TODO
+    }
+
+    public index(): void {
+        // TODO
+    }
+
+    public repository(): Array<object> {
+        // TODO
+        return [{}];
+    }
+
+    mounted() {
+        this.index();
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
